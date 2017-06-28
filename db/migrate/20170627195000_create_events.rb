80d4e4bac6d3e276064ba7ea658c9aac1e96466f
François Beausoleil <francois@teksol.info>
2017-06-27 23:12:55 -0400

    Let consumers read a total ordering for events

--- db/migrate/20170627195000_create_events.rb
+++ db/migrate/20170627195000_create_events.rb
@@ -2,6 +2,7 @@ Sequel.migration do
   change do
     create_table :events do
       column :object_id, :serial, null: false, unique: true
+      column :seq, :serial, null: false, unique: true
       column :event_id, :uuid, null: false, unique: true
       column :created_at, :timestamptz, null: false
       column :stored_at, :timestamptz, null: false, index: true, default: Sequel.function(:now)
